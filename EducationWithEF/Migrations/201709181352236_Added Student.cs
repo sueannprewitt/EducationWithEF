f67c565bf2aa2a34d3d08a92a4583e8b8b520073
Sue Ann <sueannprewitt@yahoo.com>
2017-09-18 12:30:25 -0400

    initial commit

---
+++ EducationWithEF/Migrations/201709181352236_Added Student.cs
@@ -0,0 +1,16 @@
+namespace EducationWithEF.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedStudent : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
